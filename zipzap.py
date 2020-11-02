ed58f2d4bf9f3085619ce19d29e07f19ceecf890
valeevam <72380359+valeevam@users.noreply.github.com>
2020-11-02 16:05:26 +0300

    Update zipzap.py

--- zipzap.py
+++ zipzap.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
 for i in range(0, 101):
     if i % 15 == 0:
         print('zap-zip')
-        continue
     elif i % 5 == 0:
         print('zap')
     elif i % 3 == 0:
