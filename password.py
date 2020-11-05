803156dc443976d48ab9510cd372ede787f92fc3
Марина Валеева <m.valeeva@bars.bars-kazan.ru>
2020-11-05 17:26:24 +0300

    исправлено: считать сложным пароль , состоящий как минимум из 1 буквы, 1 цифры  не короче 8 символов, можно и спец символы туда

--- password.py
+++ password.py
@@ -6,9 +6,19 @@
 включая цифры и алфавитные символы
 """
 
+	
 if __name__ == '__main__':
     pass
 
-
 passwd = input('Put password\n')
-print('good!') if passwd.isalnum and len(passwd) >= 8 else print('not good :((')
+
+found_al = False
+found_num = False
+for c in passwd: 
+    found_al = c.isalpha() 
+    if found_al: break
+for c in passwd: 
+    found_num = c.isnumeric() 
+    if found_num: break
+
+print('good!') if found_num and found_al and len(passwd) >= 8 else print('not good :((')
